5431d95becdb90c1ac38c582a41a6898ed1651e7
BigyuccaGit <80894651+BigyuccaGit@users.noreply.github.com>
2023-06-19 11:41:27 +0100

    Update and rename bme.py to weather.py

--- weather.py
+++ weather.py
@@ -1,13 +1,13 @@
 import bme280
 from machine import I2C, Pin
 
-class BME280:
+class WEATHER:
     def __init__(self, sda = 14, scl = 15, freq = 400000):
         self.i2c=I2C(1, sda=Pin(sda), scl=Pin(scl), freq=freq)
         self.bme = bme280.BME280(i2c=self.i2c)
         
 # Read data from sensor
-    def get_data(self):
+    def get_readings(self):
     
         t, p, h = self.bme.read_compensated_data()
  
@@ -16,8 +16,8 @@ class BME280:
         h /= 1024
         
         measurements={}
-        measurements["T"] = t
-        measurements["P"] = p
-        measurements["H"] = h
+        measurements["Temperature"] = t
+        measurements["Pressure"] = p
+        measurements["Humidity %"] = h
         
         return measurements
