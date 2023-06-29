d2d92342bc7bc76ff4a4787d22bbef6349549e2a
Howard Huckle <howardhuckle@yahoo.co.uk>
2023-06-29 17:56:01 +0100

    tested

--- read_vsys.py
+++ read_vsys.py
@@ -1,33 +1,9 @@
 import machine
-import sys
-import time
 
 def measure_vsys():
-    Pin(25, Pin.OUT, value=1)
-    Pin(29, Pin.IN, pull=None)
+    machine.Pin(25, machine.Pin.OUT, value=1)
+    machine.Pin(29, machine.Pin.IN, pull=None)
     reading = machine.ADC(3).read_u16() * 9.9 / 2**16
-    Pin(25, Pin.OUT, value=0, pull=Pin.PULL_DOWN)
-    Pin(29, Pin.ALT, pull=Pin.PULL_DOWN, alt=7)
+    machine.Pin(25, machine.Pin.OUT, value=0, pull=machine.Pin.PULL_DOWN)
+    machine.Pin(29, machine.Pin.ALT, pull=machine.Pin.PULL_DOWN, alt=7)
     return reading
-
-while True:
-    r= measure_vsys()
-    print(f"VSYS {round(r,1)} {r}")
-    time.sleep(2)
-
-# print("sys.implementation:{}".format(sys.implementation))
-# print("sys.version:{}".format(sys.version))
-# wl_cs = machine.Pin(25) # WiFi chip SDIO_DATA3 / gate on FET between VSYS divider (FET drain) and GPIO29 (FET source)
-# print("Initial WL_CS (GPIO25) state:{}".format(wl_cs))
-# wl_cs.init(mode=machine.Pin.OUT, value=1)
-# pin = machine.ADC(29)
-# adc_reading  = pin.read_u16()
-# adc_voltage  = (adc_reading * 3.3) / 65535
-# vsys_voltage = adc_voltage * 3
-# print("""ADC reading:{}
-# ADC voltage:{}
-# VSYS voltage:{}""".format(adc_reading, adc_voltage, vsys_voltage))
-# wl_cs.init(mode=machine.Pin.ALT, pull=machine.Pin.PULL_DOWN, alt=31)#try to restore initial WL_CS state
-
-
-
