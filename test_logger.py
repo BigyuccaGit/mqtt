3545edeba9f2cb5c4486be4789936836c58a9671
H E Huckle <howardhuckle@yahoo.co.uk>
2023-07-28 17:04:47 +0100

    1st logger version

---
+++ test_logger.py
@@ -0,0 +1,15 @@
+from logger import LOGGER
+
+logger=LOGGER()
+
+logger.info("Hello", "World")
+logger.warn("The", "quick", "brown", "fox")
+logger.error("Long Message", "Short Message")
+logger.debug("X")
+
+print("-"*80)
+
+for line in logger.iterate():
+    print(line)
+
+logger.clear()
