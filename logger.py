efcbfa1abad3e34bd5b43b435fbf7a78062d63f7
H E Huckle <howardhuckle@yahoo.co.uk>
2023-08-01 20:45:11 +0100

    Changed time format; Dealt with starting conditions

--- logger.py
+++ logger.py
@@ -9,8 +9,12 @@ def log(level, text):
 #    dt = machine.RTC().datetime()
     dt = time.gmtime()
  #   datetime = "{0:04d}-{1:02d}-{2:02d} {4:02d}:{5:02d}:{6:02d}".format(*dt)
-    datetime = "{0:04d}-{1:02d}-{2:02d} {3:02d}:{4:02d}:{5:02d}".format(*dt)
-    log_entry = "{0} [{1:7} /{2:>8}] {3} {4}".format(datetime, level, gc.mem_free(), text,  os.stat(_logfile_)[6])
+    datetime = "{0:04d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:02d}".format(*dt)
+    start_size = os.stat(_logfile_)[6]
+    log_entry_part1 = "{0} [{1:7} /{2:>8}] {3}".format(datetime, level, gc.mem_free(), text)
+    end_size = start_size + len(log_entry_part1) + 2*6 + 1
+#    log_entry = "{0} [{1:7} /{2:>8}] {3} {5} {5}".format(datetime, level, gc.mem_free(), text,  start_size, end_size)
+    log_entry = log_entry_part1 + "{0:6}{1:6}".format(start_size, end_size)
     print(log_entry)
     with open(_logfile_, "a") as f:
         f.write(log_entry + '\n')
@@ -44,8 +48,8 @@ def init():
         info("Log file exists")
         return
     else:
-        info("Creating empty log file")
         clear()
+        info("Created empty log file")
         
    
         
