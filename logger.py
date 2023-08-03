4feedf70bf6c7bb668d835d39373def819279e99
H E Huckle <howardhuckle@yahoo.co.uk>
2023-08-03 16:10:37 +0100

    corrected log record count

--- logger.py
+++ logger.py
@@ -1,20 +1,23 @@
 import machine, gc, os, time
 
+no_records = 0
 _logfile_ = "logfile.txt"
 def file(logfile):
     global _logfile_
     _logfile_ = logfile
     
 def log(level, text):
+    global no_records
 #    dt = machine.RTC().datetime()
+    no_records += 1
     dt = time.gmtime()
  #   datetime = "{0:04d}-{1:02d}-{2:02d} {4:02d}:{5:02d}:{6:02d}".format(*dt)
-    datetime = "{0:04d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:02d}".format(*dt)
+    datetime = "{0:04d}/{1:02d}/{2:02d} {3:02d}:{4:02d}:{5:02d}Z".format(*dt)
     start_size = os.stat(_logfile_)[6]
     log_entry_part1 = "{0} [{1:7} /{2:>8}] {3}".format(datetime, level, gc.mem_free(), text)
-    end_size = start_size + len(log_entry_part1) + 2*6 + 1
+    end_size = start_size + len(log_entry_part1) + 2*6 + 5 + 1
 #    log_entry = "{0} [{1:7} /{2:>8}] {3} {5} {5}".format(datetime, level, gc.mem_free(), text,  start_size, end_size)
-    log_entry = log_entry_part1 + "{0:6}{1:6}".format(start_size, end_size)
+    log_entry = log_entry_part1 + "{0:6}{1:6}{2:5}".format(start_size, end_size, no_records)
     print(log_entry)
     with open(_logfile_, "a") as f:
         f.write(log_entry + '\n')
@@ -32,6 +35,8 @@ def debug(*items):
     log("debug", " ".join(map(str, items)))
     
 def clear():
+    global no_records
+    no_records = 0
     with open(_logfile_, "w") as f:
         pass
         
