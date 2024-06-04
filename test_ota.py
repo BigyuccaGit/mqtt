1e049b11775b5636ba3b0d8e08507eb19a1c6b24
H E Huckle <howardhuckle@yahoo.co.uk>
2024-06-04 16:58:37 +0100

    using dummy file

--- test_ota.py
+++ test_ota.py
@@ -1,8 +1,8 @@
 from ota import OTAUpdater
 from constants import ssid, password
 
-firmware_url = "https://github.com/BigyuccaGit/mqtt"#"https://github.com/kevinmcaleer/ota_test/main/"
+firmware_url = "https://github.com/BigyuccaGit/mqtt/"#"https://github.com/kevinmcaleer/ota_test/main/"
 
-ota_updater = OTAUpdater(ssid, password, firmware_url, "test_ota.py")
+ota_updater = OTAUpdater(ssid, password, firmware_url, "dummy.py")
 
 ota_updater.download_and_install_update_if_available()
