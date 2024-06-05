eb3250e2b2a1a32781881e78e1c23a02306cf9d9
H E Huckle <howardhuckle@yahoo.co.uk>
2024-06-05 15:14:35 +0100

    Changed various names to ease readability

--- ota.py
+++ ota.py
@@ -6,33 +6,35 @@ import machine
 from time import sleep
 
 class OTAUpdater:
+    
     """ This class handles OTA updates. It connects to the Wi-Fi, checks for updates, downloads and installs them."""
     def __init__(self, ssid, password, repo_url, filename):
         self.filename = filename
         self.ssid = ssid
         self.password = password
         self.repo_url = repo_url
+        self.origin ="master/"
         if "www.github.com" in self.repo_url :
             print(f"Updating {repo_url} to raw.githubusercontent")
             self.repo_url = self.repo_url.replace("www.github","raw.githubusercontent")
         elif "github.com" in self.repo_url:
             print(f"Updating {repo_url} to raw.githubusercontent'")
             self.repo_url = self.repo_url.replace("github","raw.githubusercontent")            
-        self.version_url = self.repo_url + 'master/' + 'version.json'
-        print(f"version url is: {self.version_url}")
-        self.firmware_url = self.repo_url + 'master/'+ filename
+        self.version_on_repo_url = self.repo_url + self.origin + 'version.json'
+        print(f"version url is: {self.version_on_repo_url}")
+        self.filename_on_repo_url = self.repo_url + self.origin + filename
 
-        # get the current version (stored in version.json)
+        # get the current version (stored in version.json) in memory
         if 'version.json' in os.listdir():    
             with open('version.json') as f:
-                self.current_version = int(json.load(f)['version'])
-            print(f"Current device firmware version is '{self.current_version}'")
+                self.current_version_in_memory = int(json.load(f)['version'])
+            print(f"Current device filename version is '{self.current_version_in_memory}'")
 
         else:
-            self.current_version = 0
+            self.current_version_in_memory = 0
             # save the current version
             with open('version.json', 'w') as f:
-                json.dump({'version': self.current_version}, f)
+                json.dump({'version': self.current_version_in_memory}, f)
             
     def connect_wifi(self):
         """ Connect to Wi-Fi."""
@@ -45,49 +47,49 @@ class OTAUpdater:
             sleep(0.25)
         print(f'Connected to WiFi, IP is: {sta_if.ifconfig()[0]}')
         
-    def fetch_latest_code(self)->bool:
+    def fetch_latest_code_from_repo(self)->bool:
         """ Fetch the latest code from the repo, returns False if not found."""
         
         # Fetch the latest code from the repo.
-        response = urequests.get(self.firmware_url)
+        response = urequests.get(self.filename_on_repo_url)
         if response.status_code == 200:
-            print(f'Fetched latest firmware code, status: {response.status_code}, -  {response.text}')
+            print(f'Fetched latest filename code from repo, status: {response.status_code}, -  {response.text}')
     
             # Save the fetched code to memory
-            self.latest_code = response.text
+            self.latest_code_from_repo = response.text
             return True
         
         elif response.status_code == 404:
-            print(f'Firmware not found - {self.firmware_url}.')
+            print(f'Filename not found - {self.filename_on_repo_url}.')
             return False
 
     def update_no_reset(self):
         """ Update the code without resetting the device."""
 
         # Save the fetched code and update the version file to latest version.
-        with open('latest_code.py', 'w') as f:
-            f.write(self.latest_code)
+        with open('latest_code_from_repo.py', 'w') as f:
+            f.write(self.latest_code_from_repo)
         
         # update the version in memory
-        self.current_version = self.latest_version
+        self.current_version_in_memory = self.latest_version_from_repo
 
         # save the current version
         with open('version.json', 'w') as f:
-            json.dump({'version': self.current_version}, f)
+            json.dump({'version': self.current_version_in_memory}, f)
         
         # free up some memory
-        self.latest_code = None
+        self.latest_code_from_repo = None
 
         # Overwrite the old code.
-#         os.rename('latest_code.py', self.filename)
+#         os.rename('latest_code_from_repo.py', self.filename)
 
     def update_and_reset(self):
         """ Update the code and reset the device."""
 
-        print(f"Updating device... (Renaming latest_code.py to {self.filename})", end="")
+        print(f"Updating device... (Renaming latest_code_from_repo.py to {self.filename})", end="")
 
         # Overwrite the old code.
-        os.rename('latest_code.py', self.filename)  
+        os.rename('latest_code_from_repo.py', self.filename)  
 
         # Restart the device to run the new code.
         print('Restarting device...')
@@ -99,21 +101,21 @@ class OTAUpdater:
         # Connect to Wi-Fi
         self.connect_wifi()
 
-        print(f'Checking for latest version... on {self.version_url}')
-        response = urequests.get(self.version_url)
+        print(f'Checking for latest version... on {self.version_on_repo_url}')
+        response = urequests.get(self.version_on_repo_url)
         print(response.text)
         data = json.loads(response.text)
         
-        print(f"data is: {data}, url is: {self.version_url}")
+        print(f"data is: {data}, url is: {self.version_on_repo_url}")
         print(f"data version is: {data['version']}")
         # Turn list to dict using dictionary comprehension
 #         my_dict = {data[i]: data[i + 1] for i in range(0, len(data), 2)}
         
-        self.latest_version = int(data['version'])
-        print(f'latest version is: {self.latest_version}')
+        self.latest_version_from_repo = int(data['version'])
+        print(f'latest version is: {self.latest_version_from_repo}')
         
         # compare versions
-        newer_version_available = True if self.current_version < self.latest_version else False
+        newer_version_available = True if self.current_version_in_memory < self.latest_version_from_repo else False
         
         print(f'Newer version available: {newer_version_available}')    
         return newer_version_available
@@ -121,7 +123,7 @@ class OTAUpdater:
     def download_and_install_update_if_available(self):
         """ Check for updates, download and install them."""
         if self.check_for_updates():
-            if self.fetch_latest_code():
+            if self.fetch_latest_code_from_repo():
                 self.update_no_reset() 
                 self.update_and_reset() 
         else:
--- test_ota.py
+++ test_ota.py
@@ -1,8 +1,8 @@
 from ota import OTAUpdater
 from constants import ssid, password
 
-firmware_url = "https://github.com/BigyuccaGit/mqtt/"#"https://github.com/kevinmcaleer/ota_test/main/"
+repo_url = "https://github.com/BigyuccaGit/mqtt/"#"https://github.com/kevinmcaleer/ota_test/main/"
 
-ota_updater = OTAUpdater(ssid, password, firmware_url, "dummy.py")
+ota_updater = OTAUpdater(ssid, password, repo_url, "dummy.py")
 
 ota_updater.download_and_install_update_if_available()
