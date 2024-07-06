87b87b9a82453117a6ad8ce1e601851853e9755b
Howard Huckle <howardhuckle@yahoo.co.uk>
2024-07-06 10:32:01 +0100

    3rd test

--- blink.py
+++ blink.py
@@ -2,7 +2,8 @@ from machine import Pin
 from time import sleep
 
 pin=Pin("LED",Pin.OUT)
-print("Hello")       
+print("Hello")
+print("Hello Again")       
 while True:
     pin.toggle()
     sleep(1)
