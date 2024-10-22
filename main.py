2a7e7aee331b21f7c383eba9effadc46e50b7855
H.E.Huckle <howardhuckle@yahoo.co.uk>
2024-10-22 10:35:09 +0100

    updated main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ MQTT doc
 https://pypi.org/project/micropython-umqtt.simple/
 
 """
+import machine
 import logger
 # Ensure logger exists
 logger.init()
@@ -242,10 +243,10 @@ def main_loop():
         except KeyboardInterrupt as e:
             raise KeyboardInterrupt
             
-#         except Exception as e:
-#             logger.error(f'Unanticipated Exception: {e} {repr(e)}')
-#             logger.error("Will attempt to reconnect in",wifi_retry, minutes[wifi_retry != 1])
-#             time.sleep(wifi_retry * 60)
+        except Exception as e:
+            logger.error(f'Unanticipated Exception: {e} {repr(e)}')
+            logger.error("Will attempt to reconnect in",wifi_retry, minutes[wifi_retry != 1])
+            time.sleep(wifi_retry * 60)
 
 # Finally, start running code
 
