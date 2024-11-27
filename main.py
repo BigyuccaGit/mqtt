58044742ca26dc24b2ee3fa6aefd160a7b806736
H.E.Huckle <howardhuckle@yahoo.co.uk>
2024-11-27 16:06:59 +0000

    Loadable copy of main

--- main.py
+++ main.py
@@ -31,16 +31,30 @@ import network
 from ota import OTAUpdater
 from picotemp import picotemp
 from connect_to_wifi import connect_to_wifi
+import saverestore as dct
+
+# Get parameters
+params = dct.restore()
 
 # Interval between measurements / retrys (minutes)
-interval = 15
-wifi_retry = 2
+interval : int = params["interval"] #15
+
+wifi_retry : int = params["wifi_retry"] #2
 
 # Poll interval looking for subscriptions (seconds)
-subscription_period = 5
+sub_poll : int = params["sub_poll"] #5
 
 # Interval between drift corrections using NTP (hrs)
-drift_correction_interval_hrs = 24
+drift_correction : int = params["drift_correction"] # 24
+
+# Default quality of service
+qos : int = params["qos"] # 1
+
+# Daylight threshold (volts)
+daylight_threshold = params["daylight_threshold"]
+
+# Low Voltage threshold (volts)
+voltage_threshold = params["voltage_threshold"]
 
 # Define various exceptions
 class ForceRestart(Exception):
@@ -72,21 +86,24 @@ def mqtt_subscription_callback(topic, message):
     logger.info(f'Topic \"{topic.decode("utf-8")}\", message \"{message_s}\"')  # Debug print out of what was received over MQTT
     
     if topic == b'exit':
+        logger.info("Forcing exit")
         raise ForceExit
     
     elif topic == b'interval':
-        interval=float(message)
-        print("Processed interval ------------", interval)
+        interval=int(message)
+        params["interval"] = interval
+        logger.info("Processed interval ------------", interval)
     
     elif topic == b'sub_poll':
-        print(message)
-        subscription_period = float(message)
+        sub_poll = int(message)
         timer.deinit()
-        timer.init(period = int(1000*subscription_period), callback =  lambda timer : mqtt_client.check_msg())
+        timer.init(period = int(1000*sub_poll), callback =  lambda timer : mqtt_client.check_msg())
 #        timer.init(period = int(1000*subscription_period), callback =  poll_for_subscriptions)
-        print("Processed sub_poll ------------", subscription_period)
+        params["sub_poll"] = sub_poll
+        logger.info("Processed sub_poll ------------", sub_poll)
         
     elif topic == b'restart':
+        logger.info("Forcing restart")
         raise ForceRestart
     
     elif topic == b'ota':
@@ -95,10 +112,38 @@ def mqtt_subscription_callback(topic, message):
         repo_url = "https://github.com/BigyuccaGit/mqtt/"#"https://github.com/kevinmcaleer/ota_test/main/"
         ota_updater = OTAUpdater(repo_url)
         ota_updater.loop_over_updates()
-        
+
+    elif topic == b'qos':
+        qos = int(message)
+        params["qos"] = qos
+        logger.info("Processed qos ------------", qos) 
+
+    elif topic == b'drift_correction':
+        drift_correction = int(message)  
+        params["drift_correction"] =  drift_correction
+        logger.info("Processed drift_correction interval ----- ", drift_correction)    
+
+    elif topic == b'wifi_retry':
+        wifi_retry = int(message)
+        params["wifi_retry"] = wifi_retry
+        logger.info("Wifi retry interval (mins) ----- ", wifi_retry)
+
+    elif topic == b'daylight_threshold':
+        daylight_threshold = float(message)  
+        params["daylight_threshold"] =  daylight_threshold
+        logger.info("Processed daylight threshold ----- ", daylight_threshold)  
+
+    elif topic == b'voltage_threshold':
+        voltage_threshold = float(message)  
+        params["voltage_threshold"] =  voltage_threshold
+        logger.info("Processed voltage threshold ----- ", voltage_threshold)    
+
     else:
         logger.error(f"Unknown topic {topic} received")
   
+    # Save any changes
+    dct.save(params)
+
 """ Setup client and connect to mqtt server """
 def connect_to_mqtt_server():
     
@@ -119,6 +164,7 @@ def connect_to_mqtt_server():
     # Connect to the MQTT server
     logger.info("Connecting to mqtt_client")
     mqtt_client.connect()
+    mqtt_client.publish("/connected", "", qos = qos)
     
     return mqtt_client
 
@@ -138,53 +184,62 @@ def publish_loop(mqtt_client, weather, last_ntp_setting):
             
         raw=weather.get_readings()   
         
-        # Perform low pass filtering and add
+        # Perform low pass filtering and add to dictionary
         raw['T_FILTER'] = t_filter.calc(raw["Temperature"])
         raw['P_FILTER'] = p_filter.calc(raw["Pressure"])
         raw['H_FILTER'] = h_filter.calc(raw["Humidity %"])
-
-        # Convert to JSON format
-        payload = ujson.dumps(raw)
-        
-        # Publish the data
-        logger.info("Publish weather", payload)
-        mqtt_client.publish(mqtt_publish_topic, payload, qos = 1)
   
-        # Publish aux data
-        vsys = read_vsys()
+        # Get aux data
+        vsys = read_vsys() 
         light = ldr.read_u16() * conv
         pico_temp = picotemp()
+
+        # Get derived values
+        daylight =  1 if (light >= daylight_threshold) else 0
+        low_voltage = 0 if vsys >= voltage_threshold else 1
 
         v_filt = v_filter.calc(vsys)
         l_filt = l_filter.calc(light)
         pico_temp_filt = picotemp_filter.calc(pico_temp)
-        
-        aux = {"Voltage": vsys, "Light" : light, "Picotemp" : pico_temp,
-               "V_FILTER": v_filt, "L_FILTER": l_filt, "PICO_TEMP" : pico_temp_filt}
-        payload = ujson.dumps(aux)
-        
-        logger.info("Publish auxiliary data", payload)
-        mqtt_client.publish("/auxiliary", payload, qos = 1)
-        
 
-                    
+           # Convert raw weather data to JSON format
+        weather_payload = ujson.dumps(raw)
+        
+        # Determine qos level
+        qos_level = qos if daylight else 0
+
+        # Publish the weather data
+        logger.info("Publish weather", weather_payload)
+        mqtt_client.publish(mqtt_publish_topic, weather_payload, qos = qos_level)
+    
+        # Convert aux data to JSON format
+        aux = {"Voltage": vsys, "Light" : light, "Picotemp" : pico_temp,
+               "V_FILTER": v_filt, "L_FILTER": l_filt, "PICO_TEMP" : pico_temp_filt,
+               "DAYLIGHT": daylight}
+        aux_payload = ujson.dumps(aux)
+        
+        logger.info("Publish auxiliary data", aux_payload)
+        mqtt_client.publish("/auxiliary", aux_payload, qos = qos_level)
+               
         # Announce delay before next reading
-        logger.info("Next sample in",interval,minutes[interval != 1])
+        this_interval = interval if not low_voltage else 2 * interval
+        y,m,d,h,min,s,_,_ = time.gmtime(time.time() + this_interval * 60)
+        logger.info("Next sample in", this_interval, f"{minutes[interval != 1]} @ {y}/{m:02d}/{d:02d} {h:02d}:{min:02d}:{s:02d}Z")
 
         # Send all of log
         for line in logger.iterate():
-            mqtt_client.publish("/pico_log", line, qos = 1)
+            mqtt_client.publish("/pico_log", line, qos = qos_level)
         # Then clear it
         logger.clear()
   
         # Delay
-        time.sleep(interval * 60)
+        time.sleep(this_interval * 60)
         
          # Perform time drift correction if required
         tdif = time.time() - last_ntp_setting
         
 #        print("DEBUG", tdif, drift_correction_interval_hrs * 3600, tdif + last_ntp_setting, last_ntp_setting )
-        if tdif >= drift_correction_interval_hrs * 3600:
+        if tdif >= drift_correction * 3600:
             ntp_time = ntp.gettime()
             if ntp_time != 0:
                 ntp.setRTC(ntp_time)
@@ -215,28 +270,26 @@ def main_loop():
             # Get NTP time
             ntp.settime()
             last_ntp_setting = time.time()
-            logger.info(f"Time will be checked for drift every {drift_correction_interval_hrs} hrs")
+            logger.info(f"Time will be checked for drift every {drift_correction} hrs")
             
             # Setup client connection to mqtt server 
             mqtt_client = connect_to_mqtt_server()
             
              # Once connected, subscribe to the MQTT topics
             logger.info("Subscribing")
-            mqtt_client.subscribe("exit")
-            mqtt_client.subscribe("interval")
-            mqtt_client.subscribe("sub_poll")
-            mqtt_client.subscribe("restart")
-            mqtt_client.subscribe("ota")
-            mqtt_client.subscribe("/weather_ack")
+            topics = ("exit", "interval", "sub_poll", "restart", "ota", "qos",
+                       "drift_correction", "wifi_retry", "daylight_threshold", "voltage_threshold")
+            for topic in topics:
+                mqtt_client.subscribe(topic)
             
             logger.info("Wait 1 second to settle")
             time.sleep(1)
             
             # Start polling for subscriptions
-            logger.info("Start polling for subscriptions every", 1000*subscription_period, "ms")
+            logger.info("Start polling for subscriptions every", 1000 * sub_poll, "ms")
             timer = Timer()
-            timer.init(period = 1000*subscription_period, callback =  lambda timer : mqtt_client.check_msg())
-#            timer.init(period = int(1000*subscription_period), callback = poll_for_subscriptions)
+            timer.init(period = 1000 * sub_poll, callback =  lambda timer : mqtt_client.check_msg())
+#            timer.init(period = int(1000 * sub_poll), callback = poll_for_subscriptions)
                     
             # Commence loop over readings
             logger.info("Commence publishing loop")
@@ -251,7 +304,6 @@ def main_loop():
         except OSError as e:
             logger.error(f'OSError: {e} -> {errortext[e.errno]}')
       #      logger.error(f'OSError: {e}')
-
             logger.error("Will attempt to reconnect in",wifi_retry,minutes[wifi_retry != 1])
             time.sleep(wifi_retry * 60)
             
